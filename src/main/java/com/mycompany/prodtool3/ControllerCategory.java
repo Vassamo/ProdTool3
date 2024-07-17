37e0ab607fc7955fd03053677ad2e421758d2208
Vassamo <56084878+Vassamo@users.noreply.github.com>
2024-07-17 20:45:11 +0200

    ButtonsForwardTest

--- src/main/java/com/mycompany/prodtool3/ControllerCategory.java
+++ src/main/java/com/mycompany/prodtool3/ControllerCategory.java
@@ -7,6 +7,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 public class ControllerCategory {
@@ -18,15 +22,20 @@ public class ControllerCategory {
     private Button addCategoryButton;
     @FXML
     private Button addProductButton;
+    @FXML
+    private GridPane mainGrid;
 
     private CategoryClass rootCategory;
+    private CategoryClass currentCategory;
 
     public void initialize() {
         // Inicjalizacja danych i ustawienie TreeView
         rootCategory = createSampleData();
+        currentCategory = rootCategory;
         TreeItem<String> rootItem = createTreeItem(rootCategory);
         categoryTreeView.setRoot(rootItem);
         categoryTreeView.setShowRoot(true);
+        updateMainGrid();
 
         categoryTreeView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue != null) {
@@ -34,7 +43,7 @@ public class ControllerCategory {
                 CategoryClass selectedCategory = findCategoryByName(rootCategory, selectedCategoryName);
                 if (selectedCategory != null) {
                     // Ustawienie nazw produktów w ListView
-                    List<String> productNames = selectedCategory.getProductsNames(); // Załóżmy, że getProductsNames() zwraca nazwy produktów
+                    List<String> productNames = selectedCategory.getProductsNames();
                     productListView.setItems(FXCollections.observableArrayList(productNames));
                 }
             }
@@ -48,6 +57,7 @@ public class ControllerCategory {
             ControllerAddCategoryDialog dialog = new ControllerAddCategoryDialog(getStage(), selectedCategory);
             dialog.show();
             refreshTreeView();
+            updateMainGrid();
         }
     }
 
@@ -87,7 +97,7 @@ public class ControllerCategory {
 
     private TreeItem<String> createTreeItem(CategoryClass category) {
         TreeItem<String> item = new TreeItem<>(category.getName());
-        item.setExpanded(true); // Opcjonalnie, aby rozwijać elementy po dodaniu
+        item.setExpanded(true);
         for (CategoryClass subcategory : category.getSubcategories()) {
             item.getChildren().add(createTreeItem(subcategory));
         }
@@ -125,4 +135,35 @@ public class ControllerCategory {
     private Stage getStage() {
         return (Stage) addCategoryButton.getScene().getWindow();
     }
+
+private void updateMainGrid() {
+    mainGrid.getChildren().clear();
+    List<CategoryClass> subcategories = currentCategory.getSubcategories();
+    int numCols = 3; // Ustaw liczbę kolumn według potrzeb
+    for (int i = 0; i < subcategories.size(); i++) {
+        CategoryClass subcategory = subcategories.get(i);
+        Button subcategoryButton = new Button(subcategory.getName());
+        subcategoryButton.setMaxWidth(Double.MAX_VALUE); // Przyciski zajmują całą dostępną szerokość
+        subcategoryButton.setMaxHeight(Double.MAX_VALUE); // Przyciski zajmują całą dostępną wysokość
+        subcategoryButton.setOnAction(e -> {
+            currentCategory = subcategory;
+            updateMainGrid();
+            refreshProductList(currentCategory);
+        });
+        mainGrid.add(subcategoryButton, i % numCols, i / numCols);
+    }
+
+    for (int col = 0; col < numCols; col++) {
+        ColumnConstraints colConstraints = new ColumnConstraints();
+        colConstraints.setHgrow(Priority.ALWAYS);
+        mainGrid.getColumnConstraints().add(colConstraints);
+    }
+
+    for (int row = 0; row < (subcategories.size() / numCols) + 1; row++) {
+        RowConstraints rowConstraints = new RowConstraints();
+        rowConstraints.setVgrow(Priority.ALWAYS);
+        mainGrid.getRowConstraints().add(rowConstraints);
+    }
+}
+
 }
